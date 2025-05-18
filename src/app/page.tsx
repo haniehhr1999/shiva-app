2c76667997612540da070c3063f332620b0e3744
hanieh <haniehrahim78@gmail.com>
2025-05-18 22:29:03 +0330

    update my project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,25 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { cookies } from "next/headers";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { verifyJwt } from "@/../lib/auth";
 
-// Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-// import './styles.css';
+export default async function Home() {
 
-// import required modules
-import { Pagination } from 'swiper/modules';
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token");
+    const isVerify = await verifyJwt(token);
 
-export default function Home() {
+    if (!isVerify) {
+      redirect("login");
+    }
+  } catch (error) {
+    redirect('/login')
+    
+  }
   return (
-    <div>
-      {/* <Swiper
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper> */}
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      سلام اینجا خانه است
     </div>
   );
 }
